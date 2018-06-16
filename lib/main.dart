d75f8bd023bd0a62e5f4bc840b97e35eee1bae4a
zengzhuoyu <zengzhuoyu24@163.com>
2018-06-16 11:42:08 +0800

    16

--- lib/main.dart
+++ lib/main.dart
@@ -204,62 +204,185 @@ import 'package:flutter/material.dart';
 //  }
 //}
 
-import 'ShoppingListItem.dart';
-class ShoppingList extends StatefulWidget {
+//import 'ShoppingListItem.dart';
+//class ShoppingList extends StatefulWidget {
+//
+//  ShoppingList({Key key, this.products}) : super(key: key);
+//  final List<Product> products;
+//
+//  @override
+//  _ShoppingListState createState() => new _ShoppingListState();
+//}
+//
+//class _ShoppingListState extends State<ShoppingList> {
+//
+//  Set<Product> _shoppingCart = new Set<Product>();
+//  void _handleCartChanged(Product product, bool inCart) {
+//    setState(
+//            () {
+//          if (inCart)
+//            _shoppingCart.add(product);
+//          else
+//            _shoppingCart.remove(product);
+//        }
+//    );
+//  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text('购物清单'),
+//      ),
+//      body: new ListView(
+//        children: widget.products.map(
+//                (Product product) {
+//              return new ShoppingListItem(
+//                product: product,
+//                inCart: _shoppingCart.contains(product),
+//                onCartChanged: _handleCartChanged,
+//              );
+//            }
+//        ).toList(),
+//      ),
+//    );
+//  }
+//}
+//
+//final List<Product> _kProducts = <Product>[
+//  new Product(name: '鸡蛋'),
+//  new Product(name: '面粉'),
+//  new Product(name: '巧克力脆片'),
+//];
+//
+//void main() {
+//  runApp(
+//    new MaterialApp(
+//      title: 'Flutter教程',
+//      home: new ShoppingList(products: _kProducts),
+//    ),
+//  );
+//}
 
-  ShoppingList({Key key, this.products}) : super(key: key);
-  final List<Product> products;
+//class ContainerDemo extends StatelessWidget {
+//
+////  @override
+////  Widget build(BuildContext context) {
+////    return new Center(
+////      child: new Container(
+////        width: 128.0,
+////        height: 128.0,
+////        decoration: new BoxDecoration(
+////          color: Colors.lightBlueAccent[100],
+////        ),
+////      ),
+////    );
+////  }
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Center(
+//      child: new Container(
+//        width: 300.0,
+//        height: 400.0,
+//        decoration: new BoxDecoration(
+//          color: const Color(0xfffce5cd),
+//          border: new Border.all(
+//            color: const Color(0xff6d9eeb),
+//            width: 8.0,
+//          ),
+//        ),
+//        child: new Text('容器演示'),
+//      ),
+//    );
+//  }
+//}
+//
+//void main() {
+//  runApp(
+//    new MaterialApp(
+//      title: 'Flutter教程',
+//      home: new ContainerDemo(),
+//    ),
+//  );
+//}
 
-  @override
-  _ShoppingListState createState() => new _ShoppingListState();
-}
-
-class _ShoppingListState extends State<ShoppingList> {
-
-  Set<Product> _shoppingCart = new Set<Product>();
-  void _handleCartChanged(Product product, bool inCart) {
-    setState(
-            () {
-          if (inCart)
-            _shoppingCart.add(product);
-          else
-            _shoppingCart.remove(product);
-        }
-    );
-  }
+class ContainerDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('购物清单'),
+        title: new Text('文本控件'),
       ),
-      body: new ListView(
-        children: widget.products.map(
-                (Product product) {
-              return new ShoppingListItem(
-                product: product,
-                inCart: _shoppingCart.contains(product),
-                onCartChanged: _handleCartChanged,
-              );
-            }
-        ).toList(),
+      body: new Column(
+          children: <Widget>[
+            new Text(
+              '红色+黑色删除线+25号',
+              style: new TextStyle(
+                color: const Color(0xffff0000),
+                decoration: TextDecoration.lineThrough,
+                decorationColor: const Color(0xff000000),
+                fontSize: 25.0,
+              ),
+            ),
+            new Text(
+              '橙色+下划线+24号',
+              style: new TextStyle(
+                color: const Color(0xffff9900),
+                decoration: TextDecoration.underline,
+                fontSize: 24.0,
+              ),
+            ),
+            new Text(
+              '虚线上划线+23号+倾斜',
+              style: new TextStyle(
+                decoration: TextDecoration.overline,
+                decorationStyle: TextDecorationStyle.dashed,
+                fontSize: 23.0,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            new Text(
+              'serif字体+24号',
+              style: new TextStyle(
+                fontFamily: 'serif',
+                fontSize: 26.0,
+              ),
+            ),
+            new Text(
+              'monospace字体+24号+加粗',
+              style: new TextStyle(
+                fontFamily: 'monospace',
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            new Text(
+              '天蓝色+25号+2行跨度',
+              style: new TextStyle(
+                color: const Color(0xff4a86e8),
+                fontSize: 25.0,
+                height: 2.0,
+              ),
+            ),
+            new Text(
+              '24号+2个字母间隔',
+              style: new TextStyle(
+                fontSize: 24.0,
+                letterSpacing: 2.0,
+              ),
+            ),
+          ]
       ),
     );
   }
 }
-
-final List<Product> _kProducts = <Product>[
-  new Product(name: '鸡蛋'),
-  new Product(name: '面粉'),
-  new Product(name: '巧克力脆片'),
-];
-
 void main() {
   runApp(
     new MaterialApp(
       title: 'Flutter教程',
-      home: new ShoppingList(products: _kProducts),
+      home: new ContainerDemo(),
     ),
   );
 }
