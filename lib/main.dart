a53013681cb80dcc0c233bd61e78f2549a8bb0e3
zengzhuoyu <zengzhuoyu24@163.com>
2018-06-18 09:48:17 +0800

    22

--- lib/main.dart
+++ lib/main.dart
@@ -625,16 +625,50 @@ import 'package:flutter/material.dart';
 //  );
 //}
 
+//class LayoutDemo extends StatelessWidget {
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text('中心定位'),
+//      ),
+//      body: new Center(
+//        child: new Text('我在屏幕的中心！'),
+//      ),
+//    );
+//  }
+//}
+//
+//void main() {
+//  runApp(
+//    new MaterialApp(
+//      title: 'Flutter教程',
+//      home: new LayoutDemo(),
+//    ),
+//  );
+//}
+
 class LayoutDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('中心定位'),
+        title: new Text('对齐'),
       ),
-      body: new Center(
-        child: new Text('我在屏幕的中心！'),
+      body: new Stack(
+          children: <Widget>[
+            new Align(
+//              alignment: new FractionalOffset(0.0, 0.0),
+              alignment: FractionalOffset.topLeft,
+              child: new Image.network('http://up.qqjia.com/z/25/tu32710_10.jpg'),
+            ),
+            new Align(
+              alignment: FractionalOffset.bottomRight,
+              child: new Image.network('http://up.qqjia.com/z/25/tu32710_11.jpg'),
+            ),
+          ]
       ),
     );
   }
