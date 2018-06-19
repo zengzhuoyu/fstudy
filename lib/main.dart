7082a4f7f038d55ded908a9511fa70843eb6471b
zengzhuoyu <zengzhuoyu24@163.com>
2018-06-19 09:07:45 +0800

    25

--- lib/main.dart
+++ lib/main.dart
@@ -767,25 +767,58 @@ import 'package:flutter/material.dart';
 //  );
 //}
 
+//class LayoutDemo extends StatelessWidget {
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text('装饰容器'),
+//      ),
+//      body: new DecoratedBox(
+//        decoration: new BoxDecoration(
+//            gradient: new LinearGradient(
+//              begin: const FractionalOffset(0.0, 0.0),
+//              end: const FractionalOffset(1.0, 1.0),
+//              colors: <Color>[const Color(0xffff2cc), const Color(0xffff6eb4)],
+//            )
+//        ),
+//        child: new Container(
+//          width: 250.0,
+//          height: 250.0,
+//        ),
+//      ),
+//    );
+//  }
+//}
+//
+//void main() {
+//  runApp(
+//    new MaterialApp(
+//      title: 'Flutter教程',
+//      home: new LayoutDemo(),
+//    ),
+//  );
+//}
+
 class LayoutDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('装饰容器'),
+        title: new Text('不透明度'),
       ),
-      body: new DecoratedBox(
-        decoration: new BoxDecoration(
-            gradient: new LinearGradient(
-              begin: const FractionalOffset(0.0, 0.0),
-              end: const FractionalOffset(1.0, 1.0),
-              colors: <Color>[const Color(0xffff2cc), const Color(0xffff6eb4)],
-            )
-        ),
-        child: new Container(
-          width: 250.0,
-          height: 250.0,
+      body: new Center(
+        child: new Opacity(
+          opacity: 0.1,
+          child: new Container(
+            width: 250.0,
+            height: 100.0,
+            decoration: new BoxDecoration(
+              color: const Color(0xff000000),
+            ),
+          ),
         ),
       ),
     );
