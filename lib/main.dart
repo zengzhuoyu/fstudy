fd90351db0d7e72ba4d62c21c47c80c1aeeabc0a
zengzhuoyu <zengzhuoyu24@163.com>
2018-06-19 09:05:16 +0800

    24

--- lib/main.dart
+++ lib/main.dart
@@ -738,20 +738,54 @@ import 'package:flutter/material.dart';
 //  );
 //}
 
+//class LayoutDemo extends StatelessWidget {
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return new Scaffold(
+//      appBar: new AppBar(
+//        title: new Text('强制比例'),
+//      ),
+//      body: new AspectRatio(
+//        aspectRatio: 3.0 / 2.0,
+//        child: new Container(
+//          decoration: new BoxDecoration(
+//            color: Colors.lightBlueAccent[100],
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
+//
+//void main() {
+//  runApp(
+//    new MaterialApp(
+//      title: 'Flutter教程',
+//      home: new LayoutDemo(),
+//    ),
+//  );
+//}
+
 class LayoutDemo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('强制比例'),
+        title: new Text('装饰容器'),
       ),
-      body: new AspectRatio(
-        aspectRatio: 3.0 / 2.0,
+      body: new DecoratedBox(
+        decoration: new BoxDecoration(
+            gradient: new LinearGradient(
+              begin: const FractionalOffset(0.0, 0.0),
+              end: const FractionalOffset(1.0, 1.0),
+              colors: <Color>[const Color(0xffff2cc), const Color(0xffff6eb4)],
+            )
+        ),
         child: new Container(
-          decoration: new BoxDecoration(
-            color: Colors.lightBlueAccent[100],
-          ),
+          width: 250.0,
+          height: 250.0,
         ),
       ),
     );
